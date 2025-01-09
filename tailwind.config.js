5a0c03a9c10923e1473ac98b2d9d99cb0bea5782
teigorme <igorteixeira354@gmail.com>
2025-01-09 19:00:34 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./templates/**/*.html", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin")],
+  darkMode: "class",
+};
